90241aa65392c9799359c735e92ef4de03c047b7
Firyal Ghassari <naufal.firyal@gmail.com>
2023-11-13 18:51:37 +0700

    chore: update dependencies

--- webpack.common.js
+++ webpack.common.js
@@ -7,4 +7,16 @@ module.exports = {
       template: './src/index.html',
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource'
+      }
+    ],
+  },
 };
